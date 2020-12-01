f8d3b7d3e71f8f0dfb78e48c2740833a599e847a
rodrigosakamoto <rodosakamoto@gmail.com>
2020-12-01 13:37:48 -0300

    add page PostList

--- src/pages/PostList/index.js
+++ src/pages/PostList/index.js
@@ -1,7 +1,80 @@
-// import { Container } from './styles';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import chevronRight from '../../assets/chevronRight.svg';
+
+import {
+  Container,
+  SmallCard,
+  SmallCardInfoContainer,
+  SmallCardInfo,
+  BigCard,
+  BigCardInfoContainer,
+  BigCardInfo,
+} from './styles';
 
 function PostList() {
-  return <h1>Post List</h1>;
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    async function loadArticles() {
+      const response = api.get('/articles');
+
+      const data = (await response).data.map(article => ({
+        ...article,
+        article: article.article.replace(/(<p[^>]+?>|<p>|<\/p>)/gim, ''),
+        title: article.title.replace(/<img[^>]*>/g, ''),
+      }));
+
+      setArticles(data);
+    }
+
+    loadArticles();
+  }, []);
+
+  return (
+    <Container>
+      {articles.map((article, index) => (
+        <>
+          {(index + 1) % 3 !== 0 ? (
+            <SmallCard key={article.id} to={`/post/${article.id}`}>
+              <div className="image-container">
+                <img src={article.imageUrl} alt={article.title} />
+              </div>
+              <SmallCardInfoContainer>
+                <SmallCardInfo>
+                  <p>{article.author}</p>
+                  <h2>{article.title}</h2>
+                  <p>{article.article}</p>
+                </SmallCardInfo>
+                <div className="icon">
+                  <img src={chevronRight} alt="chevron right" />
+                </div>
+              </SmallCardInfoContainer>
+            </SmallCard>
+          ) : (
+            <BigCard key={article.id} to={`/post/${article.id}`}>
+              <div>
+                <img src={article.imageUrl} alt={article.title} />
+              </div>
+              <BigCardInfoContainer>
+                <BigCardInfo>
+                  <p>{article.author}</p>
+                  <h2>{article.title}</h2>
+                  <p>{article.article}</p>
+                </BigCardInfo>
+                <div className="icon">
+                  <img src={chevronRight} alt="chevron right" />
+                </div>
+              </BigCardInfoContainer>
+            </BigCard>
+          )}
+        </>
+      ))}
+    </Container>
+  );
 }
 
 export default PostList;
--- src/pages/PostList/styles.js
+++ src/pages/PostList/styles.js
@@ -1,3 +1,181 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const SmallCard = styled(Link)`
+  text-decoration: none;
+  width: 50%;
+  height: 320px;
+
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+
+  margin: 80px 0;
+
+  &:nth-child(6n + 1),
+  &:nth-child(6n + 2) {
+    flex-direction: row;
+  }
+
+  > div.image-container {
+    width: 100%;
+    max-width: 320px;
+    height: 320px;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+`;
+
+export const SmallCardInfoContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #fff;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+
+  position: relative;
+
+  div.icon {
+    text-decoration: none;
+    position: absolute;
+    bottom: 10%;
+    right: 5%;
+    cursor: pointer;
+
+    img {
+      width: 24px;
+      height: 16px;
+    }
+  }
+`;
+
+export const SmallCardInfo = styled.div`
+  width: 100%;
+  max-width: 480px;
+  height: 250px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  overflow: hidden;
+
+  p {
+    font-size: 24px;
+    line-height: 34px;
+    color: #2d2d2d;
+
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  h2 {
+    font-size: 36px;
+    font-weight: bold;
+    line-height: 43px;
+
+    color: #f1a10a;
+
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+`;
+
+export const BigCard = styled(Link)`
+  text-decoration: none;
+
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+
+  &:nth-child(2n) {
+    justify-content: flex-start;
+  }
+
+  img {
+    width: 100%;
+    max-width: 640px;
+    height: 640px;
+  }
+`;
+
+export const BigCardInfoContainer = styled.div`
+  max-width: 640px;
+  width: 100%;
+  height: 640px;
+
+  background: #fff;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+
+  position: relative;
+
+  > div.icon {
+    text-decoration: none;
+    position: absolute;
+    bottom: 10%;
+    right: 5%;
+    cursor: pointer;
+
+    img {
+      width: 24px;
+      height: 16px;
+    }
+  }
+`;
+
+export const BigCardInfo = styled.div`
+  max-width: 480px;
+  height: 315px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  p {
+    font-size: 24px;
+    line-height: 34px;
+    color: #2d2d2d;
+
+    display: -webkit-box;
+    -webkit-line-clamp: 5;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  h2 {
+    font-size: 36px;
+    font-weight: bold;
+    line-height: 43px;
+
+    color: #f1a10a;
+
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
